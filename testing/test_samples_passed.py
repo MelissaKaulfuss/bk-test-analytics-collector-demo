4e1f2df94937ca1c460d660ad937c56cf0c7c9b6
Pol <pzeballos@users.noreply.github.com>
2022-06-22 20:26:16 -0300

    Update test_samples_passed.py

--- testing/test_samples_passed.py
+++ testing/test_samples_passed.py
@@ -4,9 +4,9 @@ def test_uppercase():
 def test_reversed():
     assert list(reversed([1, 2, 3, 4])) == [4, 3, 2, 1]
 
-def test_some_primes():
-    assert 37 in {
-        num
-        for num in range(2, 50)
-        if not any(num % div == 0 for div in range(2, num))
-    }
+#def test_some_primes():
+#    assert 37 in {
+#        num
+#        for num in range(2, 50)
+#        if not any(num % div == 0 for div in range(2, num))
+#    }
